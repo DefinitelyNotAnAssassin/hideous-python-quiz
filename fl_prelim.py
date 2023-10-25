d98006c0af6d51981ed7bc25077750ecf7708c2f
DefinitelyNotAnAssassin <dharklike@gmail.com>
2023-10-25 21:19:41 +0800

    .....

--- fl_prelim.py
+++ fl_prelim.py
@@ -7,18 +7,18 @@ japanese = ["Wakinoshita", "Ude", "Ushiro", "Hige", "Chi", "Karada", "Hone", "No
 
 def translate(inp): 
   abcd = {
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3
   }
   
   return abcd[inp]
 
-def start_quiz():
+def start_japanese():
   score = 0
   questions_shown = []
-  for i in range(0,10):
+  for i in range(0,38):
     q = random.choice(list(set(range(0, len(english) - 1)) - set(questions_shown)))
     questions_shown.append(q)
     opt = print("Whats the Japanese Equivalent of " + english[q])
@@ -41,13 +41,72 @@ def start_quiz():
       score += 1
     else:
       print("Incorrect")
-    print("\n\n\n")
-  print("Score: " + str(score) + "/10")
+      print("Correct Answer: " + japanese[q])
+    print("\n\n")
+  #print("Score: " + str(score) + "/10")
   print("\n\n")
+  print(score)
+
+
+def start_english():
+  score = 0
+  questions_shown = []
+  for i in range(0,9):
+    q = random.choice(list(set(range(0, len(english) - 1)) - set(questions_shown)))
+    questions_shown.append(q)
+    opt = print("Whats the English Equivalent of " + japanese[q])
+    choices = [english[q]]
+    choices_int = [q]
+    for z in range (0,3):
+      x =random.choice(list(set(range(0, len(english) - 1)) - set(choices_int)))
+      choices.append(english[x])
+      choices_int.append(x)
+    shown = []
+    abcd = ['A', 'B', 'C', 'D']
+    for c in range(0,4): 
+      y = random.choice(list(set(range(0,4)) - set(shown)))
+      shown.append(y)
+      print (abcd[c] + '). ' + choices[y])
+    print()
+    ans = input("Answer: ")
+    if choices[shown[translate(ans)]] == english[q]:
+      print("Correct")
+      score += 1
+    else:
+      print("Incorrect")
+      print("Correct Answer: " + english[q])
+    print("\n\n")
+  #print("Score: " + str(score) + "/10")
+  print("\n\n")
+  print(score)
+
+
+def start_identification():
+  score = 0
+  questions_shown = []
+  for i in range(0,38):
+    q = random.choice(list(set(range(0, len(english) - 1)) - set(questions_shown)))
+    questions_shown.append(q)
+    opt = print("Whats the Japanese Equivalent of " + english[q])
+    choices = [japanese[q]]
+    choices_int = [q]
+    ans = input("Answer: ")
+    if ans == japanese[q]:
+      print("Correct")
+      score += 1
+    else:
+      print("Incorrect")
+      print("Correct Answer: " + japanese[q])
+    print("\n\n")
+  #print("Score: " + str(score) + "/10")
+  print("\n\n")
+  print(score)
+
   
 while True: 
-  start_quiz()
-  
+  #start_japanese()
+  start_english()
+  #start_identification()
   opt = input("Try again? (y/n): ")
   if opt == 'y' or opt == 'Y': 
     pass
